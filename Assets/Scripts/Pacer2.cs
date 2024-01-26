5bcdd2cbd88fb55e3cb2eec248796adb219b2bd0
Aertch <dccarlson1017@gmail.com>
2024-01-26 16:41:01 -0600

    Added shield, shield movement, and floor material

---
+++ Assets/Scripts/Pacer2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class Pacer2 : MonoBehaviour
+{
+    public float speed = 100.0f;
+    private float zMax = 7.5f;
+    private float zMin = -7.5f; //starting position
+    private int direction = 1; //positive to start
+
+    private float xMax = 7.5f;
+    private float xMin = -7.5f;
+    private int directionX = 1;
+
+    void Update()
+    {
+        float zNew = transform.position.z +
+        direction * speed * Time.deltaTime;
+        if (zNew >= zMax)
+        {
+            zNew = zMax;
+            direction *= -1;
+        }
+        else if (zNew <= zMin)
+        {
+            zNew = zMin;
+            direction *= -1;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, zNew);
+
+        float xNew = transform.position.x +
+        directionX * speed * Time.deltaTime;
+        if (xNew >= xMax)
+        {
+            xNew = xMax;
+            directionX *= -1;
+        }
+        else if (xNew <= xMin)
+        {
+            xNew = xMin;
+            directionX *= -1;
+        }
+        transform.position = new Vector3(xNew, transform.position.y, transform.position.z);
+    }
+}
